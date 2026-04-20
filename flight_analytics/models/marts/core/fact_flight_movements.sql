101a4389e34f473d801c81db9bca0b64b636d35e
Yassine <yassine.benterras@gmail.com>
2026-04-20 09:42:09 +0000

    converting the fact table into an Incremental Model

--- flight_analytics/models/marts/core/fact_flight_movements.sql
+++ flight_analytics/models/marts/core/fact_flight_movements.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
 with staging as (
     select * from {{ ref('stg_flight_states') }}
 ),
@@ -14,6 +20,11 @@ movements as (
         is_on_ground
     from staging
     where event_time is not null
+
+    {% if is_incremental() %}
+      -- This ensures we only append records newer than what is already in the table
+      and event_time > (select max(event_time) from {{ this }})
+    {% endif %}
 )
 
 select * from movements
